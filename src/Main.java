dafa31c41537d675be15b325cf16aafb0ad7dfc3
henry8581 <henry8581@yahoo.com.tw>
2017-10-17 16:56:07 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        java.util.Scanner distance = new java.util.Scanner(System.in);
+
+        double mi = distance.nextDouble();
+
+         double km = mi * 1.6;
+
+                 System.out.println("英里數:"+mi+"\r\n"+"公里數:"+km);
+
+    }
+}
